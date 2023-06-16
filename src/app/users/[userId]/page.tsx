dbdd2f4237d364ca63e8f1f920011b35308fbfb5
Abdul Hannan <abdulhani232@gmail.com>
2023-06-16 21:20:42 +0500

    added Server Components

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { Suspense, useRef } from "react";
 import getUser from "../../../../lib/getUser";
 import getUserPosts from "../../../../lib/getUserPosts";
+import UserPosts from "./Components/UserPosts";
+import { Metadata } from "next";
 
 type Params = {
   params: {
@@ -8,8 +10,31 @@ type Params = {
   };
 };
 
-export default function UserPage({ params: { userId } }: Params) {
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
   const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
 
-  return <div>page {userId} </div>;
+  return {
+    title: user.name,
+    description: `This is the ${user.name}'s page!`,
+  };
+}
+
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  const user = await userData;
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <br />
+      <Suspense fallback={<h2>Loading ...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
 }
